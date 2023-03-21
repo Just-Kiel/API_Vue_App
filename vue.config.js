6afa11450e9b8666a59be67fd6d849ba7aa07b08
Aurore <82760936+Just-Kiel@users.noreply.github.com>
2023-03-21 14:33:19 +0100

    Try of deploy

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     '@inkline/inkline'
-  ]
+  ],
+  publicPath: '/API_Vue_App/'
 })
